c1531b3b1490977771f8cebf46e11c4c44b262dc
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-26 14:12:00 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Vahtee Boo on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
